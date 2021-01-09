2fe0e4d6e62b4cba00a46d1a5b368253a274f1aa
IKDrocket <yusuke1128rocket@gmail.com>
2021-01-09 14:56:35 +0000

    Add chapter2

--- js-grammar/collection.js
+++
@@ -1,7 +0,0 @@
-var chrs = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ'];
-
-for (var i = 0; i < chrs.length; i++){
-    for (var j = 0; j < chrs.length; j++){
-        document.write('<p>' + chrs[i]+chrs[j] + '</p>');
-    }
-}
