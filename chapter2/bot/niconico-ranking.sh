205bf986049691beebe1d3aff38039ae486eaa4c
IKDrocket <yusuke1128rocket@gmail.com>
2021-01-10 18:14:36 +0900

    feat: About #1

--- chapter2/bot/niconico-ranking.sh
+++ chapter2/bot/niconico-ranking.sh
@@ -6,3 +6,6 @@ mkdir -p $dirname
 filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
 echo "Save to $filename"
 curl -s -o $filename -H "User-Agent: CrawlBot; yusuke1128rocket@gmail.com" https://www.nicovideo.jp/ranking/genre/all?rss=2.0&lang=ja-jp
+
+# $ crontab -e
+# 16 * * * * /home/vagrant/workspace/bot/niconico-ranking.sh
