b467d0a2c0fbf8510a36c8cd35bf1d346c82ec29
IKDrocket <yusuke1128rocket@gmail.com>
2021-02-04 22:15:17 +0900

    feat: http method

--- chapter3/node-js-http/index.js
+++ chapter3/node-js-http/index.js
@@ -1,17 +1,42 @@
+
 'use strict';
 const http = require('http');
-const fs = require('fs');
-const htmlFile = fs.readFileSync('./index.html', 'utf-8');
 const server = http.createServer((req, res) => {
+    const now = new Date();
+    console.info(
+        '[' + now + '] Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
-        //'Const-Type': 'text/plain; charset=utf-8'
-        'Const-Type': 'text/html; charset=utf-8'
+        'Content-Type': 'text/plain; charset=utf-8'
     });
-    //res.write(req.headers['user-agent']);
-    res.write(htmlFile);
+
+    switch (req.method){
+        case 'GET':
+            res.write('GET ' + req.url);
+            break;
+        case 'POST':
+            res.write('POST ' + req.url);
+            let rawData = '';
+            req.on('data', (chunk) => {
+                rawData = rawData + chunk;
+            }).on('end', () => {
+                console.info('[' + now + '] Data posted: ' + rawData);
+            });
+            break;
+        case 'DELETE':
+            res.write('DELETE ' + req.url);
+            break;
+        default:
+            break;
+    }
     res.end();
-});
+})
+    .on('error', e => {
+        console.error('[' + new Date() + '] Server Error', e);
+    })
+    .on('clientError', e => {
+        console.error('[' + new Date() + '] Client Error', e);
+    });
 const port = 8000;
 server.listen(port, () => {
-    console.log('listen on ' + port);
+    console.info('[' + new Date() + '] Listening on ' + port);
 });
